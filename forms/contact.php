2a7acc36242fa93757b09b930b2ff365676d0c4a
Mohammed Abdul Rahman <96978499+AbdulRahman788@users.noreply.github.com>
2024-01-14 15:30:06 +0530

    new code for contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,26 +1,22 @@
 <?php
-  
-  $receiving_email_address = 'rahmanif799@gmail.com';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+    $to = "rahmanif799@gmail.com";  
+    $subject = "New Contact Form Submission";
+    $headers = "From: $email";
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+    $email_message = "Name: $name\n";
+    $email_message .= "Email: $email\n";
+    $email_message .= "Message:\n$message";
 
-  
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+    // Check if the email is sent successfully
+    if (mail($to, $subject, $email_message, $headers)) {
+        // Use an absolute URL for the redirection
+        alert("Message Sent Successfully");
+        exit();
+    }
+}
 ?>
